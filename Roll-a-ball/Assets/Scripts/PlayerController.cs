2207b773e71fefec620d908bb526327f5a7ea3e4
Tomdarling <Jude@MEMEDA.wv.cc.cmu.edu>
2015-04-01 18:03:16 -0400

    EXAMPLE

---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Space
+	void FixedUpdate(){
+		
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f ,moveVertical);
+		
+		//GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+		//rigidbody.AddForce(movement * speed * Time.deltaTime);
+		
+	}
+}
